d4d8a54c6f7ee666e2b000442a7ba8522f8164bf
Denys <denv928@gmail.com>
2018-05-26 14:39:29 +0300

    Сайт майже зроблений залишився адаптив

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,34 @@
 $(() => {
-    new Swiper('.main-slider',{
+    new Swiper('.main-slider', {
         autoplay: {
             delay: 5000,
         },
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
     });
 });
 
+$(() => {
+    let inputVisibility = false;
+    $('.search-form__button').on('click', (e) => {
+        if (!inputVisibility) {
+            inputVisibility = true;
+            e.preventDefault();
+            $('.search-form__input').toggleClass('active');
+        } else {
+            inputVisibility = false;
+        }
+    });
+});
+
+$(() => {
+    $('.dropdown-toggle').hover(function () {
+        $(this).siblings('.dropdown-menu').addClass('active');
+    });
+
+});
